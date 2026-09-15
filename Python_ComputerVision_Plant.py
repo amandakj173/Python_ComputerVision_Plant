405b23e9ca59a005763fdd12a157b7959f609cb5
amanda173 <amandakj173@gmail.com>
2026-09-15 15:15:09 +0100

    build: data wrangling - establish test & train datasets

--- Python_ComputerVision_Plant.py
+++ Python_ComputerVision_Plant.py
@@ -16,3 +16,26 @@ train_dir = base_dir / "train_ds"
 test_dir = base_dir / "test_ds"
 
 extensions = ["*.jpg", "*.jpeg", "*.png"]  # Set allowed image types
+
+# Import train dataset
+train_ds = []
+categories = ["almond_plant", "cherry_plant", "maize_plant"]
+
+for category in categories:
+    folder_path = train_dir / category
+    for ext in extensions:
+        for file_path in folder_path.glob(ext):
+            train_ds.append({
+            "file_path": str(file_path),
+            "label": category
+            })
+
+train_df = pd.DataFrame(train_ds)
+
+# Import test dataset
+test_ds = [str(p) for p in test_dir.iterdir() if p.is_file()]
+test_df = pd.DataFrame({"file_path":test_ds})
+
+# Check imported data
+print("Train Samples:", train_df["label"].value_counts())
+print("Test Samples:", len(test_df))
